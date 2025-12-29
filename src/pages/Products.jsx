5e01ad34caa14b4f216e77344a470b0bbf5644ff
anisulislam10 <anis.inbox10@gmail.com>
2025-12-29 18:47:56 +0500

    minor chage

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -594,7 +594,7 @@ const downloadCSVTemplate = () => {
             <button className="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50">
               <ArrowDownTrayIcon className="h-5 w-5 mr-2" />
               Export
-              <svg className="ml-2 -mr-1 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+              <svg className="ml-2 -mr-1  w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                 <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
               </svg>
             </button>
@@ -602,7 +602,7 @@ const downloadCSVTemplate = () => {
               <div className="py-1">
                 <button
                   onClick={() => exportToCSV(true)}
-                  className="flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left"
+                  className="flex items-center px-4  text-sm text-gray-700 hover:bg-gray-100 w-full text-left"
                 >
                   <DocumentArrowDownIcon className="h-4 w-4 mr-2" />
                   Export All (CSV)
