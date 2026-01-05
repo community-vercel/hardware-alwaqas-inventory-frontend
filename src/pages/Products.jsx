ac632bc27182301dd2f0e98983d974789906cbeb
anisulislam10 <anis.inbox10@gmail.com>
2026-01-05 19:34:50 +0500

    minor change in UI

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -552,7 +552,7 @@ const downloadCSVTemplate = () => {
                 <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
               </svg>
             </button>
-            <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg z-10 hidden group-hover:block">
+            <div className="absolute right-0 w-48 bg-blue-100 rounded-md shadow-lg z-10 hidden group-hover:block">
               <div className="py-1">
                 <button
                   onClick={() => exportToCSV(true)}
